5b949c94d3e5417c8951af6b6ad89b3906c0118a
Lybron Sobers <lybron.developer@gmail.com>
2017-09-18 23:50:03 -0400

    Added Passport Google Auth
    Implemented authenitcation flow for Google auth.

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const Keys = require('./config/keys');
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send(
-		{
-			hi:'there',
-			apple:'Think Different'
-		}
-	)
-});
+passport.use(new GoogleStrategy({
+	clientID: Keys.DevKeys.GoogleKeys.clientID,
+	clientSecret: Keys.DevKeys.GoogleKeys.clientSecret,
+	callbackURL:'/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+	console.log('accessToken, accessToken', 'refreshToken', refreshToken, 'profile', profile);
+}));
+
+app.get('/auth/google',passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+app.get('/auth/google/callback',passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
