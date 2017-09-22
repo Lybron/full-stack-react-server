01ab2afeb25474f5029be2cc3692a7862be8f115
Lybron Sobers <lybron.developer@gmail.com>
2017-09-21 21:39:41 -0400

    Feat: Completed Auth flow - save session cookies - serialize and deserialize user info in passport

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 const express = require('express');
+const Keys = require('./config/keys');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
 require('./services/passport');
 
+mongoose.connect(Keys.DevKeys.MongoDB.mongoURI);
+
 const app = express();
+
+app.use(
+	cookieSession({
+		maxAge: 30*24*60*60*1000,
+		keys: [Keys.DevKeys.CookieKeys.sessionKey]
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,11 +1,33 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const Keys = require('../config/keys');
+
+const User = mongoose.model('users')
+
+passport.serializeUser((User, done) => {
+	done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+	User.findById(id)
+	.then(user => {
+		done(null, user);
+	});
+});
 
 passport.use(new GoogleStrategy({
 	clientID: Keys.DevKeys.GoogleKeys.clientID,
 	clientSecret: Keys.DevKeys.GoogleKeys.clientSecret,
 	callbackURL:'/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-	console.log('accessToken:, accessToken:', 'refreshToken:', refreshToken, 'profile', profile);
+	User.findOne({ googleID: profile.id })
+	.then((existingUser) => {
+		if (existingUser) {
+			done(null, existingUser);
+		} else {
+			new User({ googleID: profile.id }).save()
+			.then(user => done(null, user));
+		}
+	});
 }));
